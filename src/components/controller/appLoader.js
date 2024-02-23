6278b247238dd5e265f3508f478cedac22e3d73d
Vitali86 <vitali86@tut.by>
2024-02-23 10:42:53 +0300

    refactor: some change

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -2,6 +2,11 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
+        console.log(typeof process.env.API_URL);
+        console.log(process.env.API_KEY);
+        console.log({
+            apiKey: process.env.API_KEY,
+        });
         super(process.env.API_URL, {
             apiKey: process.env.API_KEY,
         });
--- src/components/controller/loader.js
+++ src/components/controller/loader.js
@@ -38,7 +38,10 @@ class Loader {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
-            .then((data) => callback(data))
+            .then((data) => {
+                callback(data);
+                console.log(data);
+            })
             .catch((err) => console.error(err));
     }
 }
